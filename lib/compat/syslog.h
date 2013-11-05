caf941d13a2ff1111283381f64bd7161a70104b6
Balazs Scheidler <bazsi@balabit.hu>
2013-11-05 11:47:17 +0100

    compat: add win32 implementation for syslog() constants

---
+++ lib/compat/syslog.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef COMPAT_WIN32_H_INCLUDED
+#define COMPAT_WIN32_H_INCLUDED 1
+
+#include "compat/compat.h"
+
+#ifdef _WIN32
+
+#define LOG_EMERG   0
+#define LOG_ALERT   1
+#define LOG_CRIT    2
+#define LOG_ERR     3
+#define LOG_WARNING 4
+#define LOG_NOTICE  5
+#define LOG_INFO    6
+#define LOG_DEBUG   7
+
+#define LOG_KERN    (0<<3)
+#define LOG_USER    (1<<3)
+#define LOG_MAIL    (2<<3)
+#define LOG_DAEMON  (3<<3)
+#define LOG_AUTH    (4<<3)
+#define LOG_SYSLOG  (5<<3)
+#define LOG_LPR     (6<<3)
+#define LOG_NEWS    (7<<3)
+#define LOG_UUCP    (8<<3)
+#define LOG_CRON    (9<<3)
+#define LOG_AUTHPRIV    (10<<3)
+#define LOG_FTP     (11<<3)
+
+#define LOG_PID         0x01    /* log the pid with each message */
+#define LOG_CONS        0x02    /* log on the console if errors in sending */
+#define LOG_ODELAY      0x04    /* delay open until first syslog() (default) */
+#define LOG_NDELAY      0x08    /* don't delay open */
+#define LOG_NOWAIT      0x10    /* don't wait for console forks: DEPRECATED */
+#define LOG_PERROR      0x20    /* log to stderr as well */
+
+#endif
+
+#endif
